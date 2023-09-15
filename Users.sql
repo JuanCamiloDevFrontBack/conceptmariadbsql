fcd444fe4272f29e64b2fc24760adcac6d7c6276
JuanCamiloDevFrontBack <juank0215dev@gmail.com>
2023-09-15 15:13:31 -0500

    Se modifican unos nombres de las consultas sql.

--- Users.sql
+++ Users.sql
@@ -13,46 +13,46 @@ de bases de datos, en el caso de xampp mariadb ya incorpora phpMyAdmin
 
 -- Las siguientes consultas(querys)
 -- permite mostrar las columnas de cualquier tabla con información más detallada.
-describe nameTabla;
-desc nameTabla;-- forma resumida
+describe nameTable;
+desc nameTable;-- forma resumida
 
 -- permite consultar los usuarios creados y existentes en la base de datos.
 select * from mysql.user;
 
 -- permite consultar el usuario con el cual se inicio sesión en la base de datos.
 show create user;
-show create user nameUser;-- muestra el usuario indicado(nameUser) si esta creado. (opcional)
+show create user userName;-- muestra el usuario indicado(userName) si esta creado. (opcional)
 
 -- permite crear usuarios en la base de datos. Nota las '' son obligatorias si se agrega el host.
-create user nameUser;-- crea el usuario con el host %(indica que puede acceder desde cuaquier pc)
-create user 'nameUser'@'localhost';-- crea el usuario con el host localhost(indica que puede acceder unicamente desde el pc con la ip del localhost(127.0.0.1)) (opcional)
+create user userName;-- crea el usuario con el host %(indica que puede acceder desde cuaquier pc)
+create user 'userName'@'localhost';-- crea el usuario con el host localhost(indica que puede acceder unicamente desde el pc con la ip del localhost(127.0.0.1)) (opcional)
 create user ''@'192.168.4.7';-- crea un usuario anónimo (opcional)
 
 -- verifica la existencia del usuario y en caso de que exista, lo reemplaza. (opcional)
-create or replace user nameUser;
+create or replace user userName;
 
 -- verifica la existencia del usuario y en caso de que no exista, lo crea. (opcional)
-create user if not exists nameUser;
+create user if not exists userName;
 
 -- permite crear multiples usuarios. (opcional)
-create user nameUser1, nameUser2, ..., nameUserN;
+create user userName1, userName2, ..., userNameN;
 
 -- permite crear usuarios agregando una contraseña. (opcional)
-create user nameUser identified by '123';-- las '' son obligatorias.
+create user userName identified by '123';-- las '' son obligatorias.
 select password('password to consult');-- las '' son obligatorias y muestra la contraseña codificada.
 
 -- permite crear un limite de acciones a realizar por el usuario. (opcional)
-create user nameUser with MAX_QUERIES_PER_HOUR N;-- N es el número máximo de consultas.
-create user nameUser with MAX_UPDATES_PER_HOUR N;-- N es el número máximo de updates.
-create user nameUser with MAX_CONNECTIONS_PER_HOUR N;-- N es el número máximo de conexiones.
-create user nameUser with MAX_USER_CONNECTIONS N;-- N es el número máximo de conexiones de usuarios.
-create user nameUser with MAX_STATEMENT_TIME N;-- N es el número máximo de espera para las consultas.
+create user userName with MAX_QUERIES_PER_HOUR N;-- N es el número máximo de consultas.
+create user userName with MAX_UPDATES_PER_HOUR N;-- N es el número máximo de updates.
+create user userName with MAX_CONNECTIONS_PER_HOUR N;-- N es el número máximo de conexiones.
+create user userName with MAX_USER_CONNECTIONS N;-- N es el número máximo de conexiones de usuarios.
+create user userName with MAX_STATEMENT_TIME N;-- N es el número máximo de espera para las consultas.
 
 -- permite crear usuarios indicado que la contraseña tendrá un tipo de caducidad. (opcional)
-create user nameUser password expire;
-create user nameUser password expire default;
-create user nameUser password expire never;
-create user nameUser password expire interval N day;-- N es el número de días a expirar
+create user userName password expire;
+create user userName password expire default;
+create user userName password expire never;
+create user userName password expire interval N day;-- N es el número de días a expirar
 
 /* mensaje de advertencia(warnings):
 permite mostrar las advertencias si existen despues de ejectar una consulta sql. */
@@ -175,7 +175,7 @@ select user, host from mysql.user;
 /* Nota: los usuarios que tengan un host diferente a %
 presentan algunos detalles cuando se van a renombrar o efectuar alguna acción,
 si este es el caso, a la hora de hacer cualquier cosa con esos usuarios
-se deben utilizar con la ip, por ejemplo: 'nameUser'@'ipHost'. */
+se deben utilizar con la ip, por ejemplo: 'userName'@'ipHost'. */
 
 -- 3.(da un ejemplo práctico de la nota anterior).
 /* muestra error porque los host no coinciden, esto porque al utilizar solo el nombre
@@ -211,13 +211,13 @@ select user, host, password from mysql.user;
 select * from information_schema.applicable_roles;
 
 -- permite crear un rol.
-create role nameRole;
+create role roleName;
 
 -- permite asignar un rol a las opciones que se especifican a continuación. (opcional)
-create or replace role if not exists nameRole with admin current_user;
-create or replace role if not exists nameRole with admin current_role;
-create or replace role if not exists nameRole with admin nameUser;
-create or replace role if not exists nameRole with admin nameRole;
+create or replace role if not exists roleName with admin current_user;
+create or replace role if not exists roleName with admin current_role;
+create or replace role if not exists roleName with admin userName;
+create or replace role if not exists roleName with admin roleName;
 
 -- |-----------------------------------EXAMPLE SECTION-----------------------------------|
 
@@ -248,13 +248,18 @@ genera error, pero al tener esa cláusula es trasnparente la ejecución. */
 -- Las siguientes consultas(querys)
 -- permite visualizar los provilecios otorgados a un usuario o rol.
 show grants;-- muestra los privilegios otorgados al usuario con el cual se inicio sesión.
-show grants for (nameUser OR nameRole);
+show grants for (userName OR roleName);
 -- OR
 show grants for current_user;
 show grants for current_user();
 
 -- |------grants------|
-
+-- permite otorgar permisos, privilegios y roles a un usuario.
+grant privilegeType on privilegeLevel to userName
+-- OR
+grant privilegeType on nameDatabase.* to userName
+grant privilegeType on nameDatabase.nameTable to userName
+grant privilegeType on nameDatabase.nameTable to userName
 
 -- |-----------------------------------EXAMPLE SECTION-----------------------------------|
 
@@ -272,12 +277,12 @@ show grants for devFront;-- rol creado en la sección anterior.
 
 -- Las siguientes consultas(querys)
 -- permite configurar roles por defecto al usuario con el que inicio sesión.
-set default role nameRole;
+set default role roleName;
 -- OR
 set default role none;
 
 -- permite indicar el usuario a realacionar con el rol por defecto.
-set default role nameRole for nameUser;
+set default role roleName for userName;
 
 
 -- |-----------------------------------EXAMPLE SECTION-----------------------------------|
@@ -320,10 +325,10 @@ tenga como host %, ya que a estos últimos no son necesarios especificar el host
 
 -- Las siguientes consultas(querys)
 -- permite eliminar uno o varios usuarios ya creados.
-drop user nameUser;
+drop user userName;
 -- OR
-drop user if exists nameUser;
-drop user if exists nameUser, nameUser2, nameUserN;
+drop user if exists userName;
+drop user if exists userName, userName2, userNameN;
 
 -- |-----------------------------------EXAMPLE SECTION-----------------------------------|
 
